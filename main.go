c1429484f14b733152f32646b5bf3ffab17c2bfc
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2015-05-02 23:26:47 +0900

    init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/mitchellh/cli"
+)
+
+func main() {
+	c := cli.NewCLI("app", "1.0.0")
+	c.Args = os.Args[1:]
+	c.Commands = map[string]cli.CommandFactory{
+		"foo": fooCommandFactory,
+		"bar": barCommandFactory,
+	}
+
+	exitStatus, err := c.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	os.Exit(exitStatus)
+}
