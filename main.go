bbe0c5397c7609d831b140cbb490b6a7ac8ff8b1
booyaa <booyaabooyaabooyaa@gmail.com>
2015-05-29 08:20:00 +0100

    adding power command

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 var GitCommit string
 
 const ApplicationName = "godo-cli"
-const Version = "0.0.1"
+const Version = "0.0.2"
 
 func getClinet(accessToken string) *godo.Client {
 
@@ -136,6 +136,12 @@ func main() {
 		},
 		"ssh": func() (cli.Command, error) {
 			return &command.SSHCommand{
+				Ui:     ui,
+				Client: godoCli,
+			}, nil
+		},
+		"power": func() (cli.Command, error) {
+			return &command.PowerCommand{
 				Ui:     ui,
 				Client: godoCli,
 			}, nil
