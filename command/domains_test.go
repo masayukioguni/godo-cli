ae49c5617f67ca982a67f824a6204a6e361674e5
rodolfobandeira <contato@rodolfobandeira.com>
2015-08-11 20:04:58 -0400

    Add domains_test.go

---
+++ command/domains_test.go
@@ -0,0 +1,18 @@
+package command
+
+import (
+    "reflect"
+    "testing"
+)
+
+func TestCommond_DomainsSynopsis(t *testing.T) {
+    command := &DomainsCommand{
+        Client: nil,
+    }
+
+    wantSynopsis := "Retrieve the list of domainss."
+    if !reflect.DeepEqual(command.Synopsis(), wantSynopsis) {
+        t.Errorf("DomainsCommand.Synopsis returned %+v, want %+v", command.Synopsis(), wantSynopsis)
+    }
+
+}
